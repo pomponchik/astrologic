a01bfacfb7ae30148d002c4a732911b9b064d9f5
Евгений Блинов <pomponchik@MacBook-Air-Evgenij.local>
2021-03-20 04:26:10 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="astrologic",
+    version="0.0.1",
+    author="Evgeniy Blinov",
+    author_email="zheni-b@yandex.ru",
+    description="Автоматическая оптимизация кода на уровне АСТ",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pomponchik/astrologic",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
