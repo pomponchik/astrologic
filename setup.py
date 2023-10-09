a90cc91c41157abcfc24d4e1a41025ba80bf1a2e
Evgeniy Blinov <zheni-b@yandex.ru>
2023-10-10 00:45:37 +0300

    quotes

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme_file:
+with open('README.md', 'r') as readme_file:
     readme = readme_file.read()
 
 requirements = ['astunparse==1.6.3']
 
 setup(
-    name="astrologic",
-    version="0.0.4",
-    author="Evgeniy Blinov",
-    author_email="zheni-b@yandex.ru",
-    description="Автоматическая оптимизация кода на уровне АСТ",
+    name='astrologic',
+    version='0.0.4',
+    author='Evgeniy Blinov',
+    author_email='zheni-b@yandex.ru',
+    description='Автоматическая оптимизация кода на уровне АСТ',
     long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pomponchik/astrologic",
+    long_description_content_type='text/markdown',
+    url='https://github.com/pomponchik/astrologic',
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
     ],
 )
